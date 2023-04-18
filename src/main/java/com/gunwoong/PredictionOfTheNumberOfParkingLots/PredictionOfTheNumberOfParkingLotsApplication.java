a10025026a5dabd810402ec886a3f950dae2ba29
gunwoong <gwj0421@gmail.com>
2023-04-18 16:05:50 +0900

    first commit

---
+++ src/main/java/com/gunwoong/PredictionOfTheNumberOfParkingLots/PredictionOfTheNumberOfParkingLotsApplication.java
@@ -0,0 +1,13 @@
+package com.gunwoong.PredictionOfTheNumberOfParkingLots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredictionOfTheNumberOfParkingLotsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredictionOfTheNumberOfParkingLotsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gunwoong/PredictionOfTheNumberOfParkingLots/PredictionOfTheNumberOfParkingLotsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gunwoong.PredictionOfTheNumberOfParkingLots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredictionOfTheNumberOfParkingLotsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
